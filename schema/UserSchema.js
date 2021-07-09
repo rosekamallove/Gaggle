c9f14db8f4b5cb4b911e4a59f3458eb316656604
rosekamallove <rosekamallove@gmail.com>
2021-07-09 15:21:05 +0530

    userSchemaCreated

---
+++ schema/UserSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Scheme = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+    default: "/images/profilePic.png",
+  },
+});
